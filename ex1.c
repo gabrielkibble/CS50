af55535e3c3a28d6b8163520736ac01c275471d1
gabrielkibble <gaalluki@googlemail.com>
2023-08-28 15:39:50 +0100

    New Line

--- ex1.c
+++ ex1.c
@@ -3,8 +3,9 @@
 
 int main(void) {
     int age = get_int("Enter your age: ");
-    string phone_number = get_string("Enter your phone number: ");
+    char phone_number[] = get_string("Enter your phone number: ");
     char name[] = get_string("Enter your name: ");
+    printf("Extra");
 
     printf("Age %i, Number %s, Name %s", age, phone_number, name);
 }
